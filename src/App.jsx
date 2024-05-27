cbd67bc5aa570714042e484370db3806ba1bb746
htmil <emil.cederslatt@gmail.com>
2024-05-27 02:58:12 +0200

    Created v.1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
+//*Imports
+import {Route, Routes} from 'react-router-dom'
 import './App.css'
 
-function App() {
+//*Layouts
+import Layout from './layouts/Layout'
+//*Pages
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Curriculum from './pages/Curriculum'
+import NotFound from './pages/NotFound'
 
+
+export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline bg-red-500">
-      Hello world!!
-    </h1>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="curriculum" element={<Curriculum />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   )
 }
-
-export default App
